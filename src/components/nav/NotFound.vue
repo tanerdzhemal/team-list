f520187669324fb9ada5894420758b0945c0df13
tanerdzhemal <tanerdzhemal3@gmail.com>
2023-03-17 18:34:42 +0200

    notfound component added

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>
+    Page fot found! Maybe view our
+    <router-link to="/teams">Teams</router-link>?
+  </h2>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
